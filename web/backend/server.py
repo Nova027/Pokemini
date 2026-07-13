31318c4eded7282d3cfb748686b2b17c7a7d3d43
Swarnava Das <swarnavasmailbox27071998@gmail.com>
2026-07-13 23:28:43 +0530

    refactor cleanup fix

--- web/backend/server.py
+++ web/backend/server.py
@@ -6,19 +6,20 @@ from pathlib import Path
 from web.backend.backendRouters import pokemonList
 
 BASE_DIR = Path(__file__).resolve().parent
-PROJECT_DIR = BASE_DIR.parent
+WEB_DIR = BASE_DIR.parent
+PROJECT_DIR = WEB_DIR.parent
 
 # Initialize the backend application
 app = FastAPI(title="Pokemon Battle!")
 
-app.mount("/static", StaticFiles(directory=BASE_DIR / "frontend"), name="static")
+app.mount("/static", StaticFiles(directory=WEB_DIR / "frontend"), name="static")
 
 app.include_router(pokemonList.router)
 
 # Serve the frontend entry page from the root URL
 @app.get("/")
 def read_root():
-    return FileResponse(BASE_DIR / "frontend" / "index.html")
+    return FileResponse(WEB_DIR / "frontend" / "index.html")
 
 # Define a POST endpoint that accepts JSON data
 # @app.post("/items")
